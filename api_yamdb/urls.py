978bbf50a56839b74a2c78c3cdc04e509467277d
vamotest <vamotest@gmail.com>
2020-11-11 21:27:21 +0300

    Update global project urls

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -17,8 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('redoc/', TemplateView.as_view(template_name='redoc.html'), name='redoc'),
+    path(
+        'redoc/', TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
+    path('api/v1/users/', include('users.urls')),
+    path('api/v1/auth/', include('users.urls')),
     path('api/', include('content.urls')),
 ]
