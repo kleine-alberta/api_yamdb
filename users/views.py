8166b210db635a321d8c73036a41c5e2cf4aaf73
vamotest <vamotest@gmail.com>
2020-11-23 10:28:04 +0300

    Changes after review

--- users/views.py
+++ users/views.py
@@ -7,35 +7,27 @@ from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
 
+from api_yamdb import settings
 from .models import User
 from .permissions import IsAdmin
-from .serializers import (ConfirmationCodeSerializer, UserEmailSerializer,
-                          UserSerializer)
+from .serializers import (
+    ConfirmationCodeSerializer, UserEmailSerializer, UserSerializer
+)
 
 
 @api_view(['POST'])
 def get_confirmation_code(request):
-    username = request.data.get('username')
     serializer = UserEmailSerializer(data=request.data)
-    email = request.data.get('email')
     
-    if serializer.is_valid():
-        if username:
-            username_qs = User.objects.filter(username=username)
-            email_qs = User.objects.filter(email=email)
-            user = username_qs | email_qs
-            if not user:
-                User.objects.create_user(username=username, email=email)
-            else:
-                return Response(
-                    serializer.errors, status=status.HTTP_400_BAD_REQUEST
-                )
-        user = get_object_or_404(User, email=email)
+    if serializer.is_valid(raise_exception=True):
+        username = serializer.validated_data.get('username')
+        email = serializer.validated_data.get('email')
+        user = User.objects.get_or_create(username=username, email=email)
         confirmation_code = default_token_generator.make_token(user)
         
         mail_subject = 'Код подтверждения'
         message = f'Ваш {mail_subject.lower()}: {confirmation_code}'
-        sender_email = 'Yamdb.ru <admin@yamdb.ru>'
+        sender_email = settings.DEFAULT_FROM_EMAIL
         recipient_email = email
         
         send_mail(
@@ -46,16 +38,15 @@ def get_confirmation_code(request):
             fail_silently=False
         )
         return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
 def get_jwt_token(request):
     serializer = ConfirmationCodeSerializer(data=request.data)
     
-    if serializer.is_valid():
-        email = serializer.data.get('email')
-        confirmation_code = serializer.data.get('confirmation_code')
+    if serializer.is_valid(raise_exception=True):
+        email = serializer.validated_data('email')
+        confirmation_code = serializer.validated_data.get('confirmation_code')
         user = get_object_or_404(User, email=email)
         
         if default_token_generator.check_token(user, confirmation_code):
@@ -64,8 +55,6 @@ def get_jwt_token(request):
         
         resp = {'confirmation_code': 'Неверный код подтверждения'}
         return Response(resp, status=status.HTTP_400_BAD_REQUEST)
-    
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserViewSet(viewsets.ModelViewSet):
